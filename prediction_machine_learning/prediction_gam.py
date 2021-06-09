73cd2dffa31a21a21c423eaf3845f8580927aa24
Maxime <maxwolf34@gmail.com>
2021-06-09 10:03:46 +0200

    nv dossier

---
+++ prediction_machine_learning/prediction_gam.py
@@ -0,0 +1,150 @@
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import statsmodels.api as sm
+import scipy as sp
+from pygam import LinearGAM, s, f
+
+
+# on utilise les mêmes features que dans le document prediction_no_weather
+
+def mean_absolute_percentage_error(y_true, y_pred):
+    y_true, y_pred = np.array(y_true), np.array(y_pred)
+    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+
+
+region = "ILE DE FRANCE"
+data = pd.read_csv(
+    'dataset_centrale/data/train/%s.csv' % region)['Consommation']
+data_test = pd.read_csv(
+    'dataset_centrale/data/test/%s.csv' % region)['Consommation']
+
+
+def av(weeks, data):  # moyennes sur les semaines précédentes
+    t = weeks*7*24*2
+    n = len(data)
+    v = np.zeros(n)
+    for i in range(n):
+        v[i] = np.mean(data[i-t:i])
+    return v[t:]
+
+
+def threedays(d, data):  # consommation exactement à la meme heure, d jours avant
+    n = len(data)
+    v = np.zeros(n)
+    for i in range(3*24*2, n):
+        v[i] = data[i-3*24*2]
+    return v
+
+# print(threedays(data))
+
+
+def day(data):
+    n = len(data)
+    v = np.zeros(n)
+    f = pd.read_csv(
+        'dataset_centrale/data/train/ILE DE FRANCE.csv')['weekday']
+    for i in range(n):
+        v[i] = f[i]
+    return v
+
+
+weeks = 3
+d = 3
+
+# données d'entrainement
+
+x1 = av(weeks, data)
+x2 = threedays(d, data)[weeks*24*2*7:]
+x3 = day(data)[weeks*24*2*7:]
+y = data[:]
+y = y[weeks*24*2*7:]
+
+x = np.array([x1, x2, x3]).T
+scaler = StandardScaler().fit(x)
+x = scaler.transform(x)
+
+
+# données de test
+
+x1_test = av(weeks, data_test)
+x2_test = threedays(d, data_test)[weeks*24*2*7:]
+x3_test = day(data_test)[weeks*24*2*7:]
+expected = data_test[:]
+expected = expected[weeks*24*2*7:].values
+x_test = np.array([x1_test, x2_test, x3_test]).T
+x_test = scaler.transform(x_test)
+
+
+x = np.array([x1, x2, x3]).T
+scaler = StandardScaler().fit(x)
+x = scaler.transform(x)
+
+# prédiction :
+
+g = LinearGAM(s(0) + s(1) + f(2)).fit(x, y)
+
+# print(g.summary())
+
+pred = g.predict(x_test)
+
+plt.scatter(expected, pred)
+plt.plot([4000, 12000], [4000, 12000], '--k')
+plt.title("RMS: {:.2f}".format(np.sqrt(np.mean((pred - expected) ** 2))))
+plt.xlabel('expected')
+plt.ylabel('predicted')
+
+plt.show()
+
+
+print("MAPE pour le GAM : %s" % mean_absolute_percentage_error(expected, pred))
+
+
+# on essaie de trouver le terme de pénalisation optimal (pénalisation l2)
+
+lam = np.logspace(-3, 5, 5)
+lams = [lam] * 3
+opt_gam = LinearGAM(s(0) + s(1) + f(2)).gridsearch(x, y, lam=lams)
+
+
+pred_opt = opt_gam.predict(x_test)
+
+plt.scatter(expected, pred_opt)
+plt.plot([4000, 12000], [4000, 12000], '--k')
+plt.title("RMS: {:.2f}".format(np.sqrt(np.mean((pred_opt - expected) ** 2))))
+plt.xlabel('expected')
+plt.ylabel('predicted')
+
+plt.show()
+
+print("MAPE pour le GAM optimisé : %s" %
+      mean_absolute_percentage_error(expected, pred_opt))
+
+
+lams = np.random.rand(100, 3)  # random points on [0, 1], with shape (100, 3)
+lams = lams * 6 - 3  # shift values to -3, 3
+lams = 10 ** lams  # transforms values to 1e-3, 1e3
+random_gam = LinearGAM(s(0) + s(1) + f(2)).gridsearch(x, y, lam=lams)
+
+pred_rand = random_gam.predict(x_test)
+
+plt.scatter(expected, pred_rand)
+plt.plot([4000, 12000], [4000, 12000], '--k')
+plt.title("RMS: {:.2f}".format(np.sqrt(np.mean((pred_rand - expected) ** 2))))
+plt.xlabel('expected')
+plt.ylabel('predicted')
+
+print("MAPE pour le random GAM : %s" %
+      mean_absolute_percentage_error(expected, pred_rand))
+
+
+# pour ile de France on obtient :
+# MAPE pour le GAM : 13.729216068500016
+# MAPE pour le GAM optimisé : 13.721626102151669
+# MAPE pour le random GAM : 13.709740352605523
---
+++ prediction_machine_learning/prediction_weather.py
@@ -0,0 +1,172 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import statsmodels.api as sm
+import scipy as sp
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
+
+
+# on utilisera dans la suite sm.ols pour les régressions, ça permet de voir plus facilement les paramètres qui ont
+# de l'influence sur la consommation
+
+
+region = "ILE DE FRANCE"
+
+data_test = pd.read_csv('../dataset_centrale/data/test/%s.csv' % region)
+data_train = pd.read_csv('../dataset_centrale/data/train/%s.csv' % region)
+
+features_train = data_train.iloc[:, 2:9]
+features_test = data_test.iloc[:, 2:9]
+
+conso_train = data_train['Consommation']
+conso_test = data_test['Consommation']
+
+y_train = conso_train.values
+y_test = conso_test.values
+x_train = features_train.values
+x_test = features_test.values
+
+
+#scaler = StandardScaler().fit(x_train)
+#x_train = scaler.transform(x_train)
+#x_test = scaler.transform(x_test)
+x_train = sm.add_constant(x_train)
+x_test = sm.add_constant(x_test)
+
+ols = sm.OLS(y_train, x_train).fit()
+print(ols.summary())
+
+y_pred = ols.predict(x_test)
+
+plt.scatter(y_test, y_pred)
+plt.plot([1000, 12000], [1000, 12000], '--k')
+plt.title("RMS: {:.2f}".format(np.sqrt(np.mean((y_test - y_pred) ** 2))))
+plt.xlabel('expected')
+plt.ylabel('predicted')
+plt.show()
+
+plt.plot(y_pred, label='prediction')
+plt.plot(y_test, label='actual consumption')
+plt.title('Predicted and expected consumptions')
+plt.legend()
+plt.show()
+
+
+def mean_absolute_percentage_error(y_true, y_pred):
+    y_true, y_pred = np.array(y_true), np.array(y_pred)
+    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+
+
+print('MAPE pour régression linéaire : %s' %
+      mean_absolute_percentage_error(y_test, y_pred))
+
+
+# on trouve que certains coefficients ont beaucoup d'influence que d'autres :
+# la température a une grande influence par exemple (x2)
+# la pvalue du test de student correspondant à x5 est très élevée : ce coefficient a de grandes chances d'être nul
+# x5 correspond au mois pendant lequel la consommation a été relevée.
+# le paramètre x3 est à la limite d'être considéré comme non nul, il s'agit de prate (la pluie)
+
+
+# maintenant on fait du feature engineering : on rajoute des covariables artificiellement
+
+def av(weeks, data):  # moyennes sur les semaines précédentes
+    t = weeks*7*24*2
+    n = len(data)
+    v = np.zeros(n)
+    for i in range(n):
+        v[i] = np.mean(data['Consommation'][i-t:i])
+    return v[t:]
+
+
+def threedays(d, data):  # consommation exactement à la meme heure, d jours avant
+    n = len(data)
+    v = np.zeros(n)
+    for i in range(3*24*2, n):
+        v[i] = data['Consommation'][i-3*24*2]
+    return v
+
+# print(threedays(data))
+
+
+def day(data):
+    n = len(data)
+    v = np.zeros(n)
+    f = pd.read_csv(
+        '../dataset_centrale/data/train/%s.csv' % region)['weekday']
+    for i in range(n):
+        v[i] = f[i]
+    return v
+
+
+# print(day(data))
+
+region = "ILE DE FRANCE"
+
+data_test = pd.read_csv('../dataset_centrale/data/test/%s.csv' % region)
+data_train = pd.read_csv('../dataset_centrale/data/train/%s.csv' % region)
+
+features_train = data_train.iloc[:, 2:9]
+features_test = data_test.iloc[:, 2:9]
+
+conso_train = data_train['Consommation']
+conso_test = data_test['Consommation']
+
+
+weeks = 3
+d = 3
+
+y_train = conso_train.values
+y_test = conso_test.values
+x_train = features_train.values
+x_test = features_test.values
+
+x8_train = av(weeks, data_train)
+x9_train = threedays(d, data_train)[weeks*24*2*7:]
+x10_train = day(data_train)[weeks*24*2*7:]
+
+x8_test = av(weeks, data_test)
+x9_test = threedays(d, data_test)[weeks*24*2*7:]
+x10_test = day(data_test)[weeks*24*2*7:]
+
+x_train = x_train[weeks*24*2*7:]
+x_test = x_test[weeks*24*2*7:]
+
+x_train = np.c_[x_train, x8_train, x9_train, x10_train]
+
+x_test = np.c_[x_test, x8_test, x9_test, x10_test]
+
+
+x_train = sm.add_constant(x_train)
+x_test = sm.add_constant(x_test)
+
+
+y_train = y_train[weeks*24*2*7:]
+y_test = y_test[weeks*24*2*7:]
+
+ols = sm.OLS(y_train, x_train).fit()
+print(ols.summary())
+
+y_pred = ols.predict(x_test)
+
+plt.scatter(y_test, y_pred)
+plt.plot([1000, 12000], [1000, 12000], '--k')
+plt.title("RMS: {:.2f}".format(np.sqrt(np.mean((y_test - y_pred) ** 2))))
+plt.xlabel('expected')
+plt.ylabel('predicted')
+plt.show()
+
+plt.plot(y_pred, label='prediction')
+plt.plot(y_test, label='actual consumption')
+plt.title('Predicted and expected consumptions')
+
+plt.legend()
+plt.show()
+
+print('MAPE pour régression linéaire avec features engineering : %s' %
+      mean_absolute_percentage_error(y_test, y_pred))
