40772520708b92138d4209282854e876dedd71c3
EngueVoisine <74180747+EngueVoisine@users.noreply.github.com>
2021-06-10 08:49:30 +0200

    Update SARIMAX.py

--- time_series_model/SARIMAX.py
+++ time_series_model/SARIMAX.py
@@ -16,7 +16,7 @@ from statsmodels.stats.diagnostic import acorr_ljungbox
 # In[ ]:
 
 
-#We take a look at the MAPE
+#We first introduce the MAPE
 def mean_absolute_percentage_error(y_true, y_pred):
 
     y_true, y_pred = np.array(y_true), np.array(y_pred)
@@ -26,7 +26,10 @@ def mean_absolute_percentage_error(y_true, y_pred):
 
 # In[130]:
 
-
+#We now define the Sarimax function that will forcast the number of day we want. The function is based on a 48timestep=1day loop: the function predicts for one day and
+# once the presdiction is complete, it actualizes the data needed to make the prediction. We worked on 2 different sample for the function: a sample of one week 
+#before the foracasting and a sample of one week, one year ago, before the forecasting. We kept the 2nd option as it worked better.
+#The function take much time to be ploted so I highly recommand to download the jupyter notebook file so as to see this function already ploted. 
 def Sarimax(region,jour):
     finaldf = pd.read_csv('../dataset_centrale/data/train/{}.csv'.format(region),header=0, index_col=0, parse_dates=True, squeeze=True)   
     endog=finaldf['Consommation']
@@ -37,7 +40,7 @@ def Sarimax(region,jour):
         training_obs = 30672
         training_endog = endog[training_obs-year-week+10*i:training_obs-year+10*i]
         training_mod = sm.tsa.SARIMAX(
-        training_endog, order=(25,0,25),seasonal=(25, 0, 25, 48*7*4), trend='c')
+        training_endog, order=(27,0,27),seasonal=(27, 0, 27, 48*7*4), trend='c')
         training_res = training_mod.fit()
         fcast = training_res.forecast(steps=48)
         endog=pd.concat([endog,fcast],join='inner')
@@ -48,7 +51,7 @@ def Sarimax(region,jour):
 
 
 # In[134]:
-
+#We now plot our prediction vs the true sample
 
 def SarimaxTvsP(region,jour):
     test = pd.read_csv('../dataset_centrale/data/test/{}.csv'.format(region),header=0, index_col=0, parse_dates=True, squeeze=True)
